30a964cbbab6d31d46a581f209e1b3ba0004ad00
testinglab <testinglab@testinglab-PC>
2020-01-16 11:49:59 +0530

    maven

--- com.maven.sel/src/test/java/com/maven/sel/FacebookHome.java
+++ com.maven.sel/src/test/java/com/maven/sel/FacebookHome.java
@@ -13,6 +13,8 @@ public class FacebookHome
 		  Thread.sleep(3000);
 		  driver.findElement(By.xpath("//span[@class='_1vp5']")).click(); 
 		  System.out.println("done");
+		  System.out.println("don");
+		  
 	  }
 
 }
